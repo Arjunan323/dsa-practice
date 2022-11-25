577427346148d16ad5eed80945b8ddbd6d2c5f19
Arjunan323 <uarjunan97@gmail.com>
2022-11-25 22:08:33 +0530

    assigment-1. close #1

---
+++ course-one/README.md
@@ -0,0 +1,3 @@
+## Getting Started
+
+# Coursera DSA Course
---
+++ course-one/src/RandomWord.java
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        String champion = "";
+        while (!StdIn.isEmpty()) {
+            String value = StdIn.readString();
+            i++;
+
+           if (StdRandom.bernoulli((double) 1 / i)) {
+                champion = value;
+           }
+        }
+
+        if (!champion.isEmpty()) {
+            StdOut.println(champion);
+        }
+
+    }
+
+}
